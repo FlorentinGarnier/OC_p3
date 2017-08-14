6dfa939671354bb15743059b79d0885c8dcc21aa
florentingarnier <garnier.florentin@gmail.com>
2017-08-14 23:44:29 +0200

    charting continue

--- www/src/Front/View/article/one.html.php
+++ www/src/Front/View/article/one.html.php
@@ -1,35 +1,34 @@
-
-    <div class="row">
+<div class="row">
+    <div class="page-content">
         <div class="col-xs-12">
-            <div class="starter-template">
-
-               <h2><?= $billet->getTitle() ?></h2>
-                <em>Publié le <?= $billet->getCreatedAt() ?></em>
-                <p class="lead"><?= $billet->getContent() ?></p>
-                <hr>
-            </div>
+            <h2><?= $billet->getTitle() ?></h2>
+            <em>Publié le <?= $billet->getCreatedAt() ?></em>
+            <p class="lead"><?= $billet->getContent() ?></p>
+            <hr>
         </div>
-    </div>
-    <?php if (isset($comments)) : ?>
-        <?php foreach ($comments as $comment) : ?>
+        <?php if (isset($comments)) : ?>
+            <?php foreach ($comments as $comment) : ?>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <h4><?= $comment->getFirstname() . ' ' . $comment->getLastname() ?></h4>
+                        <em>Le <?= date("d-m-Y à H:i", strtotime($comment->getCreatedAt())) ?></em>
+                        <p><?= $comment->getComment() ?></p>
+                    </div>
+                </div>
+            <?php endforeach ?>
+        <?php endif ?>
         <div class="row">
             <div class="col-xs-12">
-                <h4><?= $comment->getFirstname() . ' ' . $comment->getLastname() ?></h4>
-                <em>Le <?= date( "d-m-Y à H:i",strtotime($comment->getCreatedAt())) ?></em>
-                <p><?= $comment->getComment() ?></p>
+                <form action="#" method="post">
+                    <div class="form-group">
+                        <label hidden for="comment">Commentaire</label>
+                        <textarea class="form-control" name="comment" id="comment" cols="30" rows="10"
+                                  placeholder="Mon commentaire..."></textarea>
+                    </div>
+                    <button class="btn btn-info" type="submit">Envoyer</button>
+                </form>
             </div>
         </div>
-            <?php endforeach ?>
-    <?php endif ?>
-    <div class="row">
-        <div class="col-xs-12">
-            <form action="#" method="post">
-                <div class="form-group">
-                    <label hidden for="comment">Commentaire</label>
-                    <textarea class="form-control" name="comment" id="comment" cols="30" rows="10" placeholder="Mon commentaire..."></textarea>
-                </div>
-                <button class="btn btn-info" type="submit">Envoyer</button>
-            </form>
-        </div>
     </div>
+</div>
 
